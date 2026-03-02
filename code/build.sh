3dd3280e094108d68e9a16a7d1b1bd82a707afd0
Glynn Bird <glynn.bird@gmail.com>
2026-03-02 13:52:22 +0000

    deps

--- code/build.sh
+++ code/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npx rollup -p @rollup/plugin-commonjs -p @rollup/plugin-node-resolve -p @rollup/plugin-terser --format=es --file=../functions/api/poll.js -- poll.js
+npx rollup -p @rollup/plugin-commonjs -p @rollup/plugin-node-resolve --format=es --file=../functions/api/poll.js -- poll.js
